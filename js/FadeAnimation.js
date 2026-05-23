553ad61d6088090322c1cb1894d29b63d54843a3
PavalaPa <Valentinka.pazyuk@mail.ru>
2026-05-23 10:36:38 +0300

    fade effect

---
+++ js/FadeAnimation.js
@@ -0,0 +1,25 @@
+$(window).on("scroll", function () { // выполнение при прокрутке страницы
+    $(".fade").each(function () {
+        const top = $(this).offset().top; //расстояние от элемента до верха
+        const scroll = $(window).scrollTop(); //позиция прокрутки стр
+        const windowHeight = $(window).height(); //высота окна
+
+        if (scroll + windowHeight > top + 100) { //проверка появления эл на экране
+            $(this).animate(
+                { opacity: 1 },
+                { duration: 2000, 
+                  step: function(now) { //анимируем по шагам
+                        const translate = 60 - (60 * now);
+                        $(this).css(
+                            "transform",
+                            `translateY(${translate}px)`
+                        );
+                    },
+                    easing: "swing" //плавное ускорене
+                }
+            );
+
+            $(this).removeClass("fade"); //чтобы не было повторной анимации
+        }
+    });
+});
