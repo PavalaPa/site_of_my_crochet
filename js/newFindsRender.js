34dd96551995acea797cabc244bd5b6a7181d700
PavalaPa <Valentinka.pazyuk@mail.ru>
2026-05-29 15:44:36 +0300

    ээээ

--- js/newFindsRender.js
+++ js/newFindsRender.js
@@ -1,51 +1,107 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const grid = document.getElementById("newFindsGrid");
-    const btn = document.getElementById("showMoreBtn");
+    const btn = document.getElementById("showMoreBtnFinds");
 
-    let collections = [];
+    let finds = [];
     let visibleCount = 4;
 
-    function loadCollections() {
+    function loadFinds() {
 
         $.getJSON("/json/products.json", function(data) {
-            collections = data;
+            finds = data;
 
             render();
+            renderFavorites();
         });
     }
 
-    function render() {
+function render() {
 
-        grid.innerHTML = "";
+    grid.innerHTML = "";
 
-        const visibleItems = collections.slice(0, visibleCount);
+    const visibleItems = finds.slice(0, visibleCount);
 
-        visibleItems.forEach(col => {
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-            const card = document.createElement("div");
-            card.classList.add("newFindsCard");
+    visibleItems.forEach(col => {
 
-            card.innerHTML = `
-                <img src="${col.image}" alt="${col.name}" style="width:100%;">
+        const isFav = favorites.includes(col.id);
+
+        const card = document.createElement("div");
+        card.classList.add("newFindsCard");
+
+        card.innerHTML = `
+            <a href="/product.html?id=${col.id}" class="findContent">
+
+                <img class="favImg"
+                     src="${isFav ? '/img/icons/favorite_fill.svg' : '/img/icons/favorite.svg'}"
+                     alt="fav"
+                     style="width:32px;">
+
+                <img class="findImg" src="${col.image}" alt="${col.name}">
                 <h1>${col.name}</h1>
-                <div class="overlay">
-                    <div class="cardContent">
-                        <p>${col.description}</p>
-                    </div>
-                </div>
-                <button onclick="window.location.href=${col.link} class="BtnGo">Перейти</button>
-            `;
+            </a>
 
-            grid.appendChild(card);
+            <h2>${col.price_ru}rub</h2>
+        `;
+
+        grid.appendChild(card);
+
+            const fav = card.querySelector(".favImg"); //обработка нажатия на fav
+            fav.addEventListener("click", (event) => {
+                event.preventDefault(); //отменяем стандартное действие элемента, чтобы не переходить 
+                // на страницу товара 
+                let favorites = JSON.parse(localStorage.getItem("favorites")) || []; //парсим фав или пустой массив
+                if (favorites.includes(col.id)) {  //если товар в избранном
+                    favorites = favorites.filter(id => id !== col.id);
+                } 
+                else {
+                    favorites.push(col.id);
+                }
+                localStorage.setItem( // сохраняем
+                    "favorites",
+                    JSON.stringify(favorites)
+                );
+                render();
+                renderFavorites();
+            });
         });
 
         toggleButton();
     }
 
+    function renderFavorites() {
+
+        const menu = document.getElementById("favoritesMenu");
+
+        const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+        menu.innerHTML = "";
+
+        favorites.forEach(id => {
+
+            const product = finds.find(
+                item => item.id === id
+            );
+
+            if (product) {
+
+                const link = document.createElement("a");
+
+                link.href =
+                    `/product.html?id=${product.id}`;
+
+                link.textContent = product.name;
+
+                menu.appendChild(link);
+            }
+        });
+    }
+
     function toggleButton() {
 
-        if (collections.length > visibleCount) {
+        if (finds.length > visibleCount) {
             btn.style.display = "block";
         } else {
             btn.style.display = "none";
@@ -57,6 +113,5 @@ document.addEventListener("DOMContentLoaded", () => {
         visibleCount += 4;
         render();
     });
-
-    loadCollections();
+    loadFinds();
 });
--- js/typingText.js
+++ js/typingText.js
@@ -1,17 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const $el = $(".reviews-title"); //$ вместо document.querySelector, т.к. jquery объект
-    const text = $el.text();
-    $el.text("");
-    let i = 0;
+$(window).on("scroll", function () { // выполнение при прокрутке страницы
+    const $el = $(".reviews-title"); //jquery элемент
+    const top = $el.offset().top; //расстояние от элемента до верха
+    const scroll = $(window).scrollTop(); //позиция прокрутки стр
+    const windowHeight = $(window).height(); //высота окна
 
-    function typeWriter() {
+    if ($el.data("animated")) return; // выход если анимировали
+    if (scroll + windowHeight > top + 100) { //проверка появления эл на экране
 
-        if (i < text.length) {
-            $el.text($el.text() + text[i]);
-            i++;
-            setTimeout(typeWriter, 80);
+        $el.data("animated", true);
+
+        const text = $el.text();
+        $el.text("");
+        let i = 0;
+        function typeWriter() {
+            if (i < text.length) {
+                $el.text($el.text() + text[i]);
+                i++;
+                setTimeout(typeWriter, 80);
+            }
         }
-    }
 
-    typeWriter();
+        typeWriter();
+    }
 });
